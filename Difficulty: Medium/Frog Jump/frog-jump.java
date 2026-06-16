8e2dd384ff7bbea241be47b3fa36b2607c4c29ba
SNEKA S <sneka3920@gmail.com>
2026-06-16 10:14:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.java
+++ Difficulty: Medium/Frog Jump/frog-jump.java
@@ -1,6 +1,6 @@
 class Solution {
     int f(int i,int[] h,int[] dp){
-        if(i==0) return dp[i]=0;
+        if(i==0) return 0;
         if(dp[i]!=-1) return dp[i];
         int one=Math.abs(h[i]-h[i-1])+f(i-1,h,dp);
         int two=Integer.MAX_VALUE;
